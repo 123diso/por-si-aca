3f95c3ffcdd2fdf472446b95fc92a1159338a04e
hola <d.f9821n@gmail.com>
2023-05-17 21:32:10 -0500

    primero

---
+++ assets/script/java.js
@@ -0,0 +1,202 @@
+import { Producto, traerRopa, navigation_bar, pieDePagina, elementodelObjeto } from "./utils.js";
+
+
+
+// header y footer: Aquí me aseguro de que cargue la página 
+//y que luego se construyan la barra de navegación y el pie de página
+
+//carrusel 
+
+const alimentarRopa = async () => {
+    const base = await traerRopa();
+    //console.log(base);
+    for (let card of base) {
+        let image = new Producto(card.id, card.title, card.price, card.description, card.category, card.image); //Importa el orden
+        const carousel_men = document.querySelector("#carousel_men");
+        const carousel_women = document.querySelector("#carousel_women");
+
+        if (image.label === "men's clothing") {
+            carousel_men.appendChild(image.render());
+        } else {
+            carousel_women.appendChild(image.render());
+        };
+    }
+}
+
+alimentarRopa();
+
+const render = async () => {
+    navigation_bar();
+    pieDePagina();
+    elementodelObjeto();
+    await alimentarRopa();
+}
+
+
+window.onload = render;
+
+
+// me gusta -------------------------------------------
+
+/*let imagenes = ['https://i.pinimg.com/564x/21/d0/bd/21d0bde3ec55db35990ae5b59d32b295.jpg',
+'https://i.pinimg.com/564x/40/38/cc/4038ccbfa8b0e80bb438de1111d3d6a2.jpg',
+'https://i.pinimg.com/564x/6d/88/a6/6d88a67f13592dd5ccfed12b19925789.jpg',
+'https://i.pinimg.com/564x/aa/32/6d/aa326d91cff3f9ff0a3519aeefdfd030.jpg',
+'https://i.pinimg.com/564x/80/61/7d/80617dd6e3de3856a71f4f3e6a5ce078.jpg',
+'https://i.pinimg.com/564x/7e/9b/09/7e9b092dc6b1cf3fcfdbd042220a7b5b.jpg'];*/
+
+//compra-------------------------------------------
+
+//const co = document.querySelector("#desplegar1");
+//const abrir = document.querySelector("#abrir");
+//const cerrar = document.querySelector("#cerrar");
+
+/*abrir.addEventListener("click", () => {
+
+    co.classList.add("visible");
+
+})
+
+cerrar.addEventListener("click", () => {
+
+    co.classList.remove("visible");
+
+})
+
+// Tarjeta -------------------------------------------
+
+let previewContainer = document.querySelector('.products-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.movimiento1 .imgproducto ').forEach(imgproducto => {
+    imgproducto.onclick = () => {
+        previewContainer.style.display = 'flex';
+        let name = imgproducto.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close => {
+    close.querySelector('.fa-rectangle-xmark').onclick = () => {
+        close.classList.remove('active');
+        previewContainer.style.display = 'none';
+    };
+});
+
+// añadir a compra segundo intento -------------------------------------------
+
+const cartitem = document.querySelector('.cartunion');
+const rowproduct = document.querySelector('.cart-item');
+
+const productList = document.querySelector('.products-preview ');
+
+let allProducts = [];
+
+const valorTotal = document.querySelector('.subtotal')
+
+const countProducts = document.querySelector('#contador-productos')
+
+productList.addEventListener('click', e => {
+    if (e.target.classList.contains('bt-a')) {
+        const product = e.target.parentElement
+
+        const infoProduct = {
+            img: product.querySelector('img').textContent,
+            quantify: 1,
+            title: product.querySelector('h3').textContent,
+            price: product.querySelector('span').textContent,
+            info: product.querySelector('p').textContent,
+        }
+
+        // Aumento cantidad al carrito
+
+        const exits = allProducts.some(product => product.title === infoProduct.title)
+        console.log(exits)
+
+        if (exits) {
+            const products = allProducts.map(product => {
+                if (product.title === infoProduct.title) {
+                    product.quantify++;
+                    return product
+                } else {
+                    return product
+                }
+            })
+            allProducts = [...products]
+        } else {
+
+            allProducts = [...allProducts, infoProduct]
+
+        }
+
+        showHtml();
+    }
+
+})
+// Eliminar del carrito
+
+rowproduct.addEventListener('click', e => {
+    if (e.target.classList.contains('fa-rectangle-xmark')) {
+        const product = e.target.parentElement;
+        const title = product.querySelector('p').textContent;
+
+        allProducts = allProducts.filter(
+            product => product.title !== title
+
+        );
+
+        console.log(allProducts)
+        showHtml()
+    }
+
+});
+
+// añadir carrito mostrar html
+
+const showHtml = () => {
+
+    // Limpiar carrito
+
+    rowproduct.innerHTML = '';
+
+    let total = 0;
+    let totalOfProducts = 0;
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cartunion');
+
+        containerProduct.innerHTML = `
+
+    <div class="cart-item">
+
+    <div  ><i  class="fa-solid fa-rectangle-xmark" ></i></div>
+        <img src="${product.img}" alt="">
+        <div class="detalles"> 
+            <h3>${product.title}</h3>
+            <p>${product.info}</p>
+            <span class="cantidad">cantidad : ${product.quantify}</span>
+            <div class="price"> <span>${product.price}</span></div>
+        </div>
+    </div>        
+        
+        `;
+
+        rowproduct.append(containerProduct);
+
+        total = total + product.quantify * parseInt(product.price.slice(1));
+
+        totalOfProducts = totalOfProducts + product.quantify;
+
+    });
+
+    valorTotal.innerText = `$${total}`;
+
+    countProducts.innerText = totalOfProducts;
+
+};*/
---
+++ assets/script/utils.js
@@ -0,0 +1,210 @@
+
+export const traerRopa = async () => {
+    const response = await fetch("https://raw.githubusercontent.com/matilocker/Proyecto-final/main/Assets/Script/ropa.json");
+    const data = await response.json();
+    //console.log(data);
+    return data;
+};
+
+export class Producto {
+    titulo = "";
+    id = "";
+    imagen = "";
+    descripcion = "";
+    label = "";
+    precio = "";
+    constructor(id, title, price, description, category, image) {
+        this.id = id;
+        this.titulo = title;
+        this.precio = price;
+        this.label = category;
+        this.descripcion = description;
+        this.imagen = image;
+    }
+
+    render() {
+        const imagen = document.createElement("img");
+        const caja = document.createElement("div");
+        caja.classList.add("image");
+
+        imagen.src = this.imagen;
+        caja.appendChild(imagen);
+        return caja;
+    };
+
+    addClickListener() {
+        const image = document.querySelector("#image" + this.id);
+        image.addEventListener("click", () => {
+            window.location = "/Producto.html?productId=" + this.id;
+            const img = document.createElement("img");
+            img.src = "image"
+            window.appendChild("img");
+        });
+    };
+}
+
+export const navigation_bar = async () => { //Orgullosísimo porque esto lo escribí solo
+    const header = await document.querySelector(".nav_bar");
+
+    const logo = document.createElement("a");
+    logo.href = "Landing.html";
+    const logo_imagen = document.createElement("img");
+    logo_imagen.src = "Logo/Logo nav.png";
+
+    const navegador = document.createElement("nav");
+    navegador.classList.add("nav");
+
+    logo.appendChild(logo_imagen);
+    header.appendChild(logo);
+    header.appendChild(navegador);
+    
+
+    const buscar = document.createElement("input");
+    buscar.classList.add("buscar");
+    buscar.type = "text";
+    buscar.placeholder = "buscar";
+
+    const nosotros = document.createElement("a");
+    nosotros.href = "contact.html";
+    nosotros.classList.add("nosotros");
+    nosotros.innerText = "¿Quiénes somos?";
+
+    const tienda = document.createElement("a");
+    tienda.href = "Landing.html";
+    tienda.classList.add("tienda");
+    tienda.innerText = "Tienda";
+
+    const favoritos = document.createElement("div");
+    favoritos.classList.add("favoritos");
+    favoritos.id = "abrirgustar";
+    favoritos.innerHTML = `
+<i class="fa-regular fa-heart"></i>`+ " Favoritos";
+
+    const carrito = document.createElement("a");
+    carrito.classList.add("carrito");
+    carrito.id = "abrircarrito";
+    carrito.innerHTML = `
+<i class="fa-solid fa-bag-shopping"></i>`+ " Carrito";
+
+    const login = document.createElement("a");
+    login.href = "Login.html";
+    login.innerHTML = `
+<i class="fa-regular fa-user"></i>`+ " Login";
+
+    const signup = document.createElement("a");
+    signup.href = "Registry.html";
+    signup.innerHTML = `
+<i class="fa-regular fa-user-plus"></i>`+ " Sign up";
+
+const userName = document.createElement("a");
+userName.href = "Registry.html";
+userName.innerHTML = `
+<i id="myUserName" class="fa-regular fa-user-plus"></i>`+ "";
+
+
+
+
+
+    navegador.appendChild(buscar);
+    navegador.appendChild(nosotros);
+    navegador.appendChild(tienda);
+    navegador.appendChild(favoritos);
+    navegador.appendChild(carrito);
+    navegador.appendChild(login);
+    navegador.appendChild(signup);
+    navegador.appendChild(userName);
+    //sconsole.log(favoritos);
+
+    const favorito =  document.querySelector(".desplegargustar");
+    const abrirgustar =  document.getElementById("abrirgustar");
+    
+
+    // Abre el modal al hacer clic en el botón
+
+    abrirgustar.addEventListener("click", (e) => {
+        e.preventDefault()
+
+        favorito.style.display = 'block';
+       //console.log("hola");
+
+       const cerrargustar =  document.querySelector("#cerrargustar");
+
+       cerrargustar.onclick = () => {
+          
+          favorito.style.display = 'none';
+        }
+     }) 
+     
+
+};
+
+export const pieDePagina = async () => {
+    const footer = await document.querySelector(".footer");
+
+    const tituloPagyTarjeta = document.createElement("div");
+    tituloPagyTarjeta.classList.add("privacidad");
+    const tituloPag = document.createElement("h2");
+    tituloPag.innerText = "Nuestra página";
+    const tarjeta = document.createElement("a");
+    tarjeta.href = "";
+    tarjeta.innerHTML = `<i class="fa-regular fa-credit-card"></i>` + " Privacidad";
+
+    footer.appendChild(tituloPagyTarjeta);
+    tituloPagyTarjeta.appendChild(tituloPag);
+    tituloPagyTarjeta.appendChild(tarjeta);
+
+    const somos = document.createElement("div");
+    somos.classList.add("nosotros");
+    const parrafo = document.createElement("p");
+    parrafo.innerText = "hola"
+    const nosotros = document.createElement("a");
+    nosotros.href = "contact.html";
+    nosotros.innerText = "¿Quiénes somos?";
+
+    footer.appendChild(somos);
+    somos.appendChild(nosotros);
+    somos.appendChild(parrafo);
+
+    const contact = document.createElement("div");
+    contact.classList.add("contacto");
+    const siguenos = document.createElement("p");
+    siguenos.id = "siguenos";
+    siguenos.innerText = "¡Síguenos!";
+    const iconos = document.createElement("div");
+    iconos.classList.add("icons");
+    iconos.innerHTML = `
+    <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+    <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+    <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+    `
+    const maps = document.createElement("iframe");
+    maps.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.0334477324554!2d-76.5327906846551!3d3.341900252867967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a1eb8d09a3c1%3A0x766b9b441dfa8bbe!2sUniversidad%20ICESI!5e0!3m2!1ses!2sco!4v1679448223332!5m2!1ses!2sco";
+    maps.width = "600";
+    maps.height = "450";
+    maps.style = "border:0;";
+    maps.allowFullscreen = "true";
+    maps.loading = "lazy";
+    maps.referrerPolicy = "no-referrer-when-downgrade";
+
+    footer.appendChild(contact);
+    contact.appendChild(siguenos);
+    contact.appendChild(iconos);
+    contact.appendChild(maps);
+
+}
+
+export const elementodelObjeto = async () => {
+    
+  
+
+  }; 
+
+// export function holdName() {
+//     let name = document.getElementById("name").value;
+//     console.log("Nombre: " + name)
+  
+// }
+
+
+
+
